0510ce0e0a93d4dff611e0daa424f13091e2936e
JacobBermudes <marriagesebers@gmail.com>
2026-01-20 04:25:31 +0500

    api for balance

--- api/main.go
+++ api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
@@ -16,8 +17,16 @@ func echoHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write(body)
 }
 
+func balanceHandler(w http.ResponseWriter, r *http.Request) {
+	uid := r.URL.Query().Get("uid")
+	var balance int64 = 1122 + int64(len(uid))*33
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(balance)
+}
+
 func main() {
 	http.HandleFunc("/vb-api", echoHandler)
+	http.HandleFunc("/vb-api/balance", balanceHandler)
 	fmt.Println("Server starting on :8001")
 	http.ListenAndServe(":8001", nil)
 }
